df9bccfa03efeece2cbda56f9d60bb702e9d1a0b
Yukti Sinha <yuktisinha27@gmail.com>
2025-04-13 13:10:01 +0530

    Initial commit

---
+++ src/main/java/com/rabbitmq/ys_pub_sub/YsPubSubApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.ys_pub_sub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsPubSubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsPubSubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitmq/ys_pub_sub/YsPubSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.ys_pub_sub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsPubSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
